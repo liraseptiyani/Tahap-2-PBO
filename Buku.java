be0c366f88e9db90aa452d28b171d79440ca9c9f
liraseptiyani <112398818+liraseptiyani@users.noreply.github.com>
2023-11-29 18:11:47 +0700

    Update Buku.java
    1. Anindya Salsabila (2217051113)
    2. Intan Maghfirah (2257051015)
    3. Lira Septiyani (2217051151)

--- Buku.java
+++ Buku.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.tahap2pbo;
 
-/**
- *
- * @author asus
- */
 public class Buku {
     private String judul;
     private String pengarang;
