d11789ec06c1c6f77361af1ad64f657df54809ee
liraseptiyani <112398818+liraseptiyani@users.noreply.github.com>
2023-11-29 18:11:21 +0700

    Update Notifikasi.java
    1. Anindya Salsabila (2217051113)
    2. Intan Maghfirah (2257051015)
    3. Lira Septiyani (2217051151)

--- Notifikasi.java
+++ Notifikasi.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.tahap2pbo;
 
-/**
- *
- * @author asus
- */
 public class Notifikasi {
     private String jenis;
     private String penerima;
@@ -45,7 +37,6 @@ public class Notifikasi {
     
     public void mengirimNotifikasi(){
         System.out.println("**Notifikasi Baru**");
-
         System.out.println("Hello " + penerima + "," + isiPesan);
     }
 }
