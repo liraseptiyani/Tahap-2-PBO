610810b5e30ac5667cec83947716c5f6196294ee
liraseptiyani <112398818+liraseptiyani@users.noreply.github.com>
2023-11-29 18:13:17 +0700

    Update Pustakawan.java
    1. Anindya Salsabila (2217051113)
    2. Intan Maghfirah (2257051015)
    3. Lira Septiyani (2217051151)

--- Pustakawan.java
+++ Pustakawan.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.tahap2pbo;
 
-/**
- *
- * @author asus
- */
 public class Pustakawan {
     private String nama;
     private String nomorpegawai;
