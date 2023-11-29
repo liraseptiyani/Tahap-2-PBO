32642d3265694232fe9abf471ad3d38c85774b11
liraseptiyani <112398818+liraseptiyani@users.noreply.github.com>
2023-11-29 18:12:59 +0700

    Update Pengembalian.java
    1. Anindya Salsabila (2217051113)
    2. Intan Maghfirah (2257051015)
    3. Lira Septiyani (2217051151)

--- Pengembalian.java
+++ Pengembalian.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.tahap2pbo;
 
 import java.util.Date;
 
-/**
- *
- * @author asus
- */
 public class Pengembalian {
     private Date tanggalKembali;
     private double denda;
